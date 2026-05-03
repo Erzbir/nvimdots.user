d7220a34fbe57d5319c276c9c8cfc52b94167389
Erzbir <github@erzbir.com>
2026-05-03 16:06:01 +0800

    update

--- configs/editor/nvim-surround.lua
+++ configs/editor/nvim-surround.lua
@@ -1,17 +1,3 @@
 return function()
-	require("nvim-surround").setup({
-		keymaps = {
-			insert = "<C-g>s",
-			insert_line = "<C-g>S",
-			normal = "ys",
-			normal_cur = "yss",
-			normal_line = "yS",
-			normal_cur_line = "ySS",
-			visual = "s",
-			visual_line = "S",
-			delete = "ds",
-			change = "cs",
-			change_line = "cS",
-		},
-	})
+	require("nvim-surround").setup({})
 end
